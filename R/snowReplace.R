358462997ed4fd18c3863c75b3b4e2cf81dc1180
Markus Schmidberger <schmidb@ibe.med.uni-muenchen.de>
2008-12-18 02:44:40 +0000

    new snow wrapper (makeCluster, stopCluster) functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyPara@36115 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/snowReplace.R
@@ -0,0 +1,39 @@
+# snowReplace.R
+# 
+# functions for replacing snow functions to remove cluster object
+# 
+# History:
+# 17.12.2008 : Version 0.1
+#
+# Author: mschmidb
+###############################################################################
+
+makeCluster <- function(...)
+{
+	if( exists("cl", envir=.affyParaInternalEnv) ){
+		stop("In one R master instance you can only run ONE cluster!\nThere is already running another snow cluster!")	
+	}else{
+		#makeCluster
+		cl <- snow::makeCluster(...)
+		#save cl in internal environment
+		assign("cl", cl, envir=.affyParaInternalEnv)
+	}
+}
+		
+		
+stopCluster <- function(cl)
+{
+	#Get cluster object
+	if(missing(cl)){
+		if( !exists("cl", envir=.affyParaInternalEnv) )
+			stop("There is no running snow cluster!")
+		else
+			cl <- .affyParaInternalEnv$cl
+	}
+	checkCluster(cl)
+	#stopCluster
+	snow::stopCluster(cl)
+	#remove cl variable
+	if( exists("cl", envir=.affyParaInternalEnv) )
+		rm("cl", envir=.affyParaInternalEnv)
+}
