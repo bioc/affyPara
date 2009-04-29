72a3b73df03863d872f7aa300d8ec01859f9471f
Markus Schmidberger <schmidb@ibe.med.uni-muenchen.de>
2009-04-29 10:51:38 +0000

    mergeAffyBatch improved for memory amd time
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyPara@39211 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/combine.R
+++ R/combine.R
@@ -3,85 +3,83 @@
 # Functions to combine objects (in lists) after distributed computing
 #
 # History
-# 22.08.2007 : Version 0.1
-# 06.09.2007 : Version 0.2 - combineAffyBatch -> merge.AffyBatches : much faster
-# 27.11.2007 : Version 0.3 - merge.AffyBatches renamed to mergeAffyBatches
-# 06.12.2007 : Version 0.4 - code cleaning and dokumentation
+# 29.04.2009 : ... old stuff removed ...
 # 10.12.2007 : Version 0.5 - #Remove NA's from list added
 # 14.12.2007 : Version 0.6 - Input from combineMatrices changed to matrices.list
 # 07.07.2008 : Version 0.7 - mergeAffyBatches improved for great Matrices
 # 27.08.2008 : Version 0.8 - mergeAffyBatches bugfix in annotations
-# 23.03.2009 : Version 0.9 - Option verbose set to getOption("verbose") and added . to names of internatl functions
+# 23.03.2009 : Version 0.9 - Option verbose set to getOption("verbose") and added . to 
+#							  names of internatl functions
+# 29.04.2009 : Version 0.10 - mergeAffyBatch improved for memory amd time
 #
 # Copyright (C) 2008 : Markus Schmidberger <schmidb@ibe.med.uni-muenchen.de>
 ###############################################################################
 
+############################################
+# Function to merge AffyBatches
 mergeAffyBatches <- function (abatch.list,
     description = NULL, notes = character(0))
 {
-    #number auf probes in affyBatch
-    adim <- dim(intensity(abatch.list[[1]]))[1]
-	
-	#Remove NA's from list (more slaves in universum then used)
-	omit <- lapply(abatch.list, function(x){
-				if (class(x)=="AffyBatch"){
-					return(TRUE)
-				}else return(FALSE)
-			})
-	abatch.list <- abatch.list[unlist(omit)]
-	
-    #check affyBatches
-    if ( any(unlist(lapply(abatch.list,nrow)) != nrow(abatch.list[[1]])) 
-      || any(unlist(lapply(abatch.list,ncol)) != ncol(abatch.list[[1]])) )
-   		   stop("cannot merge chips of different sizes !")
-   	if ( any(unlist(lapply(abatch.list,cdfName)) != cdfName(abatch.list[[1]])))
-        warning("cdfName mismatch (using the cdfName of x)!")
-    #Check description
-    if (is.null(description)) {
-        description <- new("MIAME")
-        description@title <- "Created from merging several AffyBatches. No description was supplied. The description of the two original AffyBatches was not kept."
-    }
-    #Create phenodata
-    phenodata <- phenoData(abatch.list[[1]])
-	
-   # pData(phenodata) <- data.frame(sample=unlist(lapply(abatch.list,pData)),
-	#							   row.names=unlist(lapply(abatch.list,names<-function(data){
-    #										#row.names(pData(data))
-    #										#sampleNames(data)
-	#	
-    ##										})))
-	#		
-	if ( length(abatch.list) > 1){
+	if( length(abatch.list) == 1){
+		#if there is only on AffyBatch in the list, no merge required!
+		return(abatch.list[[1]])
+	}else{
+		#Remove non AffyBatch objects (NA's) from list (if more slaves in universum then used)
+		omit <- lapply(abatch.list, function(x){
+					if (class(x)=="AffyBatch"){
+						return(TRUE)
+					}else return(FALSE)
+				})
+		abatch.list <- abatch.list[unlist(omit)]
+
+	    #check affyBatches
+		anrow <- nrow(abatch.list[[1]])
+		ancol <- ncol(abatch.list[[1]])
+		acdfName <- cdfName(abatch.list[[1]])
+	    if ( any(unlist(lapply(abatch.list,nrow)) != anrow) 
+	      || any(unlist(lapply(abatch.list,ncol)) != ancol) )
+	   		   stop("cannot merge chips of different sizes !")
+	   	if ( any(unlist(lapply(abatch.list,cdfName)) != acdfName) )
+	        warning("cdfName mismatch (using the cdfName of x)!")
+		
+		#data from affyBatch
+		ab <- abatch.list[[1]]
+		abatch.list[[1]]<-NA
+		gc(verbose=FALSE)
+		phenodata <- phenoData(ab)
+		exprs <- intensity(ab)
+		
+	    #Check description
+	    if (is.null(description)) {
+	        description <- new("MIAME")
+	        description@title <- "Created from merging several AffyBatches. No description was supplied. The description of the two original AffyBatches was not kept."
+	    }
+
+	    #Check notes
+	    if (length(notes) == 0){
+	      notes(description) <- paste("Merge of", length(abatch.list),"AffyBatches.")
+	    } else
+			notes(description) <- notes
+
+
+		#Generat Exprs-Matrix and phenodata
 		for( k in 2:length(abatch.list)){
 			pData(phenodata) <- rbind(pData(phenodata), pData(abatch.list[[k]]))
-		}
-	}	
-		
-    #Check notes
-    if (length(notes) == 0){
-      notes(description) <- list(paste("Merge of", length(abatch.list),"AffyBatches with notes:",lapply(abatch.list,data<-function(data){
-    				notes(experimentData(data))
-    				})))
-    }
-    else
-      notes(description) <- notes
-   
-  	#Generat Exprs-Matrix
-	exprs <- intensity(abatch.list[[1]])
-	if ( length(abatch.list) > 1){
-		for( k in 2:length(abatch.list)){
 			exprs <- cbind(exprs,intensity(abatch.list[[k]]))
 			abatch.list[[k]]<-NA
+			gc(verbose=FALSE)
 		}
+		
+	    #Return ONE AffyBatch
+		exprs(ab) <- exprs
+		phenoData(ab) <- phenodata
+		experimentData(ab) <- description
+	    return(ab)
 	}
-	
-    #Return ONE AffyBatch
-    return(new("AffyBatch", exprs = exprs, 
-        phenoData = phenodata, experimentData = description, 
-        cdfName = cdfName(abatch.list[[1]]), nrow = nrow(abatch.list[[1]]), ncol = ncol(abatch.list[[1]]), 
-        annotation = abatch.list[[1]]@annotation))
 }
 
+############################################
+# Function to combine Matrices
 combineMatrices <- function(matrix.list, verbose=getOption("verbose"))
 {
 	if (verbose) cat("Rebuild matrix ")
